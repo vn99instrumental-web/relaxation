4969dd74d9c026764270f5f1f97877e433f1af30
vn99instrumental-web <vn99instrumental@gmail.com>
2026-09-14 16:55:36 +0700

    revert: restore pre-Yahoo production layout

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,6 @@ import App from './App.jsx'
 import './styles.css'
 import './journal-lock.css'
 import './brand-embrace.css'
-import './yahoo-preview.css'
-import './yahoo-layout-fix.css'
-
-document.documentElement.classList.add('yahoo-preview')
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
