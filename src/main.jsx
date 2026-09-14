c85a0f781809ab4d64dbadbcd392ea7646f4bbc6
vn99instrumental-web <vn99instrumental@gmail.com>
2026-09-14 16:42:58 +0700

    Enable Yahoo preview stylesheet

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles.css'
 import './journal-lock.css'
+import './yahoo-preview.css'
+
+document.documentElement.classList.add('yahoo-preview')
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
