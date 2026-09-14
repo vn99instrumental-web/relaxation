ebc1f5a83e7cd97259c230bd7038ff4803ff075a
vn99instrumental-web <vn99instrumental@gmail.com>
2026-09-14 16:51:53 +0700

    feat: ship Yahoo Messenger and 360 production layout

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,10 @@ import App from './App.jsx'
 import './styles.css'
 import './journal-lock.css'
 import './brand-embrace.css'
+import './yahoo-preview.css'
+import './yahoo-layout-fix.css'
+
+document.documentElement.classList.add('yahoo-preview')
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
