aedb2c745a259a89abd834b823d093578d29fcf9
vn99instrumental-web <vn99instrumental@gmail.com>
2026-09-14 17:48:38 +0700

    feat: use composite pine title header

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import './styles.css'
 import './journal-lock.css'
 import './brand-embrace.css'
+import './header-composite.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
